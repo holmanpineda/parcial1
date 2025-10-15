b3741266801663a14f3ec4e64dcdec2d0e4ad5e7
holman <pinedaholmandavid12@gmail.com>
2025-10-15 10:37:45 -0500

    parcial

---
+++ src/main/java/org/yourcompany/parcial1/Armadura.java
@@ -0,0 +1,19 @@
+package org.yourcompany.parcial1;
+
+public class Armadura {
+
+
+    public String nombre;
+    public int defensa;
+    public int ataque;
+
+    public Armadura(String nombre, int defensa, int ataque) {
+        this.nombre = nombre;
+        this.defensa = defensa;
+        this.ataque = ataque;
+
+        
+    
+
+    }       
+    } 
